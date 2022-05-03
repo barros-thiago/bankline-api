4b0a3a05249a4a56f79e27caecf442b47d50a057
barros-thiago <104264678+barros-thiago@users.noreply.github.com>
2022-05-03 20:32:18 -0300

    Criamos as classes de modelo

---
+++ src/main/java/com/dio/santander/banklineapi/Correntista.java
@@ -0,0 +1,5 @@
+package com.dio.santander.banklineapi;
+
+public class Correntista {
+
+}
---
+++ src/main/java/com/dio/santander/banklineapi/MovimentacaoTipo.java
@@ -0,0 +1,7 @@
+package com.dio.santander.banklineapi;
+
+public enum MovimentacaoTipo {
+	RECEITA,
+	DESPESA
+
+}
