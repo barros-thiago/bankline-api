43706b36392378bbe416c6dc6d3a997b245a0cbc
barros-thiago <104264678+barros-thiago@users.noreply.github.com>
2022-05-03 20:48:12 -0300

    mapeando as entidades com JPA

--- src/main/java/com/dio/santander/banklineapi/Conta.java
+++ src/main/java/com/dio/santander/banklineapi/Conta.java
@@ -1,5 +1,21 @@
 package com.dio.santander.banklineapi;
 
 public class Conta {
-
+	private Long numero;
+	private Double Saldo;
+	
+	public Long getNumero() {
+		return numero;
+	}
+	public void setNumero(Long numero) {
+		this.numero = numero;
+	}
+	public Double getSaldo() {
+		return Saldo;
+	}
+	public void setSaldo(Double saldo) {
+		Saldo = saldo;
+	}
+	
+	
 }
